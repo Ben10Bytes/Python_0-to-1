6cea9e11ed36159611e048504d3cc67d50e580c0
Ben10Byte <159570808+Ben10Byte@users.noreply.github.com>
2024-12-22 15:18:15 +0530

    know about dictonary that is dict

---
+++ Chapter 5/01_dictonary(dict).py
@@ -0,0 +1,8 @@
+marks={
+    "Bent10bytes":97,
+    "Jitu":95,
+    "Jites":84
+}
+
+#print(type(marks))
+print(marks["Bent10bytes"])
---
+++ Chapter 5/02_dict_methods.py
@@ -0,0 +1,12 @@
+marks={
+    "Bent10Bytes":100,
+    "Jitu":95,
+    "Jites":84,
+    0:"Ben10Bytes"
+}
+
+#print(marks.items())
+#print(marks.keys())
+#print(marks.values())
+marks.update({"Ben10Bytes":99,"Sachin":98})
+print(marks)
