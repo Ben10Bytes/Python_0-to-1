81ad2cd2049e8c08493dfcdf7f75cec82272556d
Ben10Byte <rk4096172@gmail.com>
2024-12-24 00:08:22 +0530

    Write a program to input eight numbers from the user and display all the unique numbers(once)

---
+++ Chapter 5PS/02_Problem2.py
@@ -0,0 +1,26 @@
+# Write a program to input eight numbers from the user and display all the unique numbers(once).
+empty_set = set()
+n1 = int(input("Enter number 1 : "))
+empty_set.add(n1)
+
+n2 = int(input("Enter number 2 : "))
+empty_set.add(n2)
+
+n3 = int(input("Enter number 3 : "))
+empty_set.add(n3)
+n4 = int(input("Enter number 4 : "))
+empty_set.add(n4)
+
+n5 = int(input("Enter number 5 : "))
+empty_set.add(n5)
+
+n6 = int(input("Enter number 6 : "))
+empty_set.add(n6)
+
+n7 = int(input("Enter number 7 : "))
+empty_set.add(n7)
+
+n8 = int(input("Enter number 8 : "))
+empty_set.add(n8)
+
+print(empty_set)
