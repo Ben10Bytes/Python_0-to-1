7f8f56868cdebb9fa46a78768746b9fb01f8ef0a
Ben10Byte <rk4096172@gmail.com>
2024-12-23 23:30:55 +0530

    Creating a Hindi Dictionary
    Write a program to create a dictionary of Hindi words with values as their English translation. Provide user with an option to look it up!

---
+++ Chapter 5PS/01_Problem1.py
@@ -0,0 +1,11 @@
+# Write a program to create a dictionary of Hindi words with values as their English translation. Provide user with an option to look it up!
+
+Words={
+    "Achha": "Good",
+    "Bura": "Bad",
+    "Sundar": "Beautiful",
+    "Humesa": "Always"
+}
+
+Word=input("Which word you want to mean : ")
+print(Words[Word])
