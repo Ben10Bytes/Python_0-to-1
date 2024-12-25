2599a1613294130bca35e2f999185c665f95f533
Ben10Byte <rk4096172@gmail.com>
2024-12-26 00:53:04 +0530

    Never change the value inside a list which is contained in set

---
+++ Chapter 5PS/09_Problem9.py
@@ -0,0 +1,5 @@
+# Can you chnage the values inside a list which is contained in set S
+
+S = {8,7,12,"Ben10Bytes", [1,2]}  # List cannot included in a Set
+# List are mutable and not hashable.
+# Sets are immutable and hashable
