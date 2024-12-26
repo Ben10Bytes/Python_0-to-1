035cb877b317f151c53ca7f13bb57a7c306269d2
Ben10Byte <rk4096172@gmail.com>
2024-12-26 20:57:56 +0530

    Write a program to print yes when age entered by the user is greater than or equal to 18

---
+++ Chapter 6/Quick_quiz.py
@@ -0,0 +1,10 @@
+# Write a program to print yes when age entered by the user is greater than or equal to 18.
+
+a = int(input("Enter the age : "))
+
+if (a>=18):
+    print("Yes")
+    print("Thank You for joining our club")
+
+else:
+    print("No")
