d1588d0e3aba639d9e01f352346b6a4a3d0f8718
Ben10Byte <rk4096172@gmail.com>
2024-12-26 20:51:54 +0530

    if else statement and if elif else ladder

--- Chapter 6/02_if_elif_else_ladder.py
+++ Chapter 6/02_if_elif_else_ladder.py
@@ -1,13 +1,18 @@
 
 a = int(input("Enter your age : "))
 
-# if else statement
+# If elif else ladder
 
 
 if (a>=18):
     print("You are above the age of consent")
     print("Good for you")
 
+elif (a<0):
+    print("The age you have entered does not exist")
+
+elif (a==0):
+    print("You are entering 0 which is not a valid age")    
 
 else:
     print("You are below the age of consent")
