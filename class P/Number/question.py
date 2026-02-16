bace45f39cf3516c42f920fd79d8d59735cf156f
Ben10Byte <rk4096172@gmail.com>
2026-02-17 03:05:04 +0530

    question for check the type of number

---
+++ class P/Number/question.py
@@ -0,0 +1,18 @@
+"""
+Inside the editor, complete the following steps:
+
+1). Create a variable x and assign it the integer 5
+2). Create a variable y and assign it the float 3.14
+3). Create a variable z and assign it the complex number 2+3j
+4). Print the type of each variable using type()
+
+"""
+x = 5
+y = 3.14
+z = 2 + 3j
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+print(type(x), type(y), type(z))
