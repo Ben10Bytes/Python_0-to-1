4f59180cf9d4929505f0e120111e977b5b93b033
Ben10Byte <rk4096172@gmail.com>
2026-02-20 16:59:30 +0530

    add  something in "in'' operator

--- Strings/in.py
+++ Strings/in.py
@@ -1,4 +1,28 @@
-# x is asign like x = 'a', x ='b', etc... that means 1 by 1
+"""
+
+1). x is asign like x = 'a', x ='b', etc... that means 1 by 1 in each of the chracter will billow of the word
 
 for x in "banana":
     print(x)
+
+    Output will be:
+                    b
+                    a
+                    n
+                    a
+                    n
+                    a
+
+
+2).
+
+txt = "The life lesson is depend on your thought"
+print("depend" in txt)
+
+    Output will be:
+                    True
+
+
+
+"""
+
