453a35b20712c813bc6673766a00e3b3531be376
Ben10Byte <rk4096172@gmail.com>
2026-02-21 18:08:45 +0530

    learn some methods in python

--- Strings/Modify_String_3.py
+++ Strings/Modify_String_3.py
@@ -2,8 +2,9 @@ a = "Forgive me!"
 print(a.upper()) # All word change in capital letter
 print(a.lower()) # All word change in small letter
 print(a.capitalize()) # Starting of the letter change in capital and rest will be small
-print(a.title()) # 
-print(a.find("g"))
+print(a.title()) # The first letter of each word will be capitalized 
+print(a.find("g")) # Find index position
+print(a.find("z")) # If not find index position the compiler return -1
 
 b= "rAJAN"
 print(b.capitalize())
