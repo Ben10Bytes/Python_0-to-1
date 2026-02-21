ca9443983ce13c40d159227d82c0e4ab89ba7c58
Ben10Byte <rk4096172@gmail.com>
2026-02-21 18:30:50 +0530

    learn some methods

--- Strings/Modify_String_3.py
+++ Strings/Modify_String_3.py
@@ -1,3 +1,5 @@
+# Methods in Python
+
 a = "Forgive me!"
 print(a.upper()) # All word change in capital letter
 print(a.lower()) # All word change in small letter
@@ -8,3 +10,12 @@ print(a.find("z")) # If not find index position the compiler return -1
 
 b= "rAJAN"
 print(b.capitalize())
+
+c = "   Welcome to the coding world   "
+print(c.strip()) # Removing whitespace from the beginning or the end
+print(c.replace("W", "Z")) # The replace() method replaces a string with another string
+
+# I also using strip and replace methods together
+
+print(c.strip().replace("Welcome", "Go"))
+print(c.replace("Welcome", "Go").strip()) # Both line will be same
